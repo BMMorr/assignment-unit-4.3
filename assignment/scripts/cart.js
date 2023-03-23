d0dbf87b1452a4d199abe16acd442b05a451d870
Bryce Morries <brycemorries@gmail.com>
2023-03-23 11:54:20 -0500

    Completed the fuinctions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,29 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+ basket = []
+ function addItem(item) {
+    basket.push(item)
+    return true;
+ }
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+
+ function listItems(basket) {
+    for (let i=0; i < basket.length; i++) {
+        console.log(basket[i])
+    }
+    
+ }
+ listItems(basket);
+
+ function empty(basket) {
+    while (basket.length > 0) {
+        basket.pop();
+    }
+ }
+ empty(basket);
+ console.log('Check the contents of the basket after the empty function');
+ listItems(basket);
